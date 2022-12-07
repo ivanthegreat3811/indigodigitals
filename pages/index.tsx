e4edbff51c6eeb7249889b888dad8b2ac7d985b6
ivanthegreat3811 <ivan@redblackdesigns.com>
2022-12-07 14:47:51 +0800

    ci: implement husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
